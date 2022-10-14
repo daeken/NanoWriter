9c86685b78303c67b77688344bce86df21aec35a
Sera Brocious <daeken@Seras-Mac-Studio.local>
2022-10-13 21:45:17 -0400

    Initial commit

---
+++ NanoWriter/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using NanoWriter.Models;
+
+namespace NanoWriter.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser> {
+	public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+		: base(options, operationalStoreOptions) {}
+}
